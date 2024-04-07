8034aeaa36f8ea67b6cd5049941742383c4892e7
thibaudcathala <thibaud.cathala@epitech.eu>
2024-04-07 14:58:17 +0200

    add:

--- water_generator.gd
+++ water_generator.gd
@@ -25,7 +25,7 @@ func _process(_delta):
 		progresse_bar.value += 0.05
 		if progresse_bar.value >= 100:
 			progresse_bar.value = 0
-			available_water += 1
+			available_water += 10
 			text_nb_water.text = str(available_water)
 	progresse_bar_extern.value = progresse_bar.value
 	progresse_bar_extern.get_parent().visible = generate_water
