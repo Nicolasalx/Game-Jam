7525805762f4bd42e5659fc54a1cb2a7cad2ea3b
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-04-07 13:06:34 +0200

    add: healthbar

---
+++ scenes/health_bar.gd
@@ -0,0 +1,16 @@
+extends ProgressBar
+
+@onready var healthBar = $HealthBar
+
+var health: int = 100
+var time_passed: float = 0
+var update_interval: float = 2.0
+
+func _ready():
+	value = health
+
+func _process(delta):
+	time_passed += delta
+	if time_passed >= update_interval && value > 0:
+		time_passed = 0
+		value -= 1
